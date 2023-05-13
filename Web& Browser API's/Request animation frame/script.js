cb8db19712edbda4f6ead3bf64e069ba390a238b
kelebekkadircan <kelebekkadircan@gmail.com>
2023-05-13 21:57:19 +0300

    Request animation frame learning

---
+++ Web& Browser API's/Request animation frame/script.js
@@ -0,0 +1,38 @@
+const image = document.querySelector('img')
+
+let start;
+let done = false;
+
+
+
+
+function step(timestamp)
+{
+    if ( start === undefined)
+    {
+        start = timestamp;
+    }
+    
+    const elapsed = timestamp - start;
+
+    if ( elapsed > 3000)
+    {
+        done = true;
+    }
+
+    if(done)
+    {
+        return;
+    }
+
+    image.style.transform = `translateX(${elapsed / 10}px)`
+
+
+    requestAnimationFrame(step);
+}
+
+
+requestAnimationFrame(step)
+
+
+
